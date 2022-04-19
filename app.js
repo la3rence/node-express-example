c3d9c7e5af56d9dc752e21138b318b18a88c2abf
Lawrence Li <lonor@live.com>
2022-04-19 20:06:56 +0800

    feat: add middleware

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ import send from './api/send.js';
 import express from 'express';
 import logger from 'morgan';
 import sse from './api/sse.js';
+import { corsMiddleware } from './cors/wrapper.js';
 
 const app = express();
 app.use(logger('dev'));
+// to add a middleware to all requests
+app.use((req, res, next) => {
+  next();
+});
+app.use(corsMiddleware);
 app.get("/hello", hello);
 app.get("/bye", goodbye);
 app.get("/send", send);
 app.get("/sse", sse);
-
+app.get("/", (req, res) => { res.send("Hello!") });
 export default app;
--- cors/wrapper.js
+++ cors/wrapper.js
@@ -1,12 +1,5 @@
 const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // or res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
+  res = applyCorsHeaders(res);
   if (req.method === 'OPTIONS') {
     res.status(200).end()
     return
@@ -14,4 +7,26 @@ const allowCors = fn => async (req, res) => {
   return await fn(req, res)
 }
 
+const applyCorsHeaders = res => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // or res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  return res;
+}
+
+const corsMiddleware = (req, res, next) => {
+  res = applyCorsHeaders(res);
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  next()
+}
+
+export { corsMiddleware };
 export default allowCors;
