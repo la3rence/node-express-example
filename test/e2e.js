b5d4ab6b34acb6cbaa1728ee838a6895367f63a4
Lawrence Li <lonor@live.com>
2022-04-20 15:54:05 +0800

    refactor: use router

--- test/e2e.js
+++ test/e2e.js
@@ -2,6 +2,7 @@ import request from 'supertest';
 import app from '../app.js';
 import { expect } from 'chai';
 
+const BASEPATH = "/api";
 describe('E2E Test', () => {
   let server;
 
@@ -16,7 +17,7 @@ describe('E2E Test', () => {
 
   it("should send a hello", (done) => {
     request(app)
-      .get('/hello')
+      .get(BASEPATH + '/hello')
       .expect(200, (err, res) => {
         if (err) {
           return done(err);
@@ -26,14 +27,15 @@ describe('E2E Test', () => {
   })
 
   it("should send to ifttt", done => {
+    const username = process.env.USER
     request(app)
-      .get('/send?text=CI Test&link=github.com')
+      .get(BASEPATH + `/send?text=${username} is testing&link=github.com`)
       .expect(200, (err, res) => {
         if (err) {
           return done(err);
         }
         expect(res.body).to.have.property('event', 'event');
-        expect(res.body).to.have.property('text', 'CI Test');
+        expect(res.body).to.have.property('text', username + ' is testing');
         expect(res.body).to.have.property('link', 'github.com');
         expect(res.body).to.have.property('imgUrl', 'https://s3.bmp.ovh/imgs/2022/04/09/5da9d13f02880f41.png');
         expect(res.body).to.have.property('responseStatus', 200);
@@ -45,14 +47,14 @@ describe('E2E Test', () => {
     const EXPECTED_TIMEOUT = 200;
     this.timeout(EXPECTED_TIMEOUT + 100);
     const timeout = setTimeout(done, EXPECTED_TIMEOUT);
-    request(app).get('/sse').expect(200, (err, res) => {
+    request(app).get(BASEPATH + '/sse').expect(200, (err, res) => {
       clearTimeout(timeout);
       done(new Error('Unexpected Call'));
     });
   })
 
   it("should return 408 due to slow", done => {
-    request(app).get("/slow").expect(408, (err, res) => {
+    request(app).get(BASEPATH + "/slow").expect(408, (err, res) => {
       if (err) {
         return done(err);
       }
