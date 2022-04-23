80118a4bce272468474cfe5507a7a13e34e7b7c2
Lawrence Li <lonor@live.com>
2022-04-23 21:38:47 +0800

    infra: refactor

--- swagger/autogen.js
+++ swagger/autogen.js
@@ -2,7 +2,7 @@ import swaggerAutogen from 'swagger-autogen';
 
 const doc = {
   info: {
-    title: 'My API',
+    title: 'My Express API',
     description: 'Description',
   },
   host: 'localhost:8080',
@@ -10,11 +10,10 @@ const doc = {
   basePath: '/api',
 };
 
-const outputFile = './swagger-output.json';
+const outputFile = './swagger/output.json';
 const endpointsFiles = ['./app.js'];
 
 /* NOTE: if you use the express Router, you must pass in the 
    'endpointsFiles' only the root file where the route starts,
    such as index.js, app.js, routes.js, ... */
-
-swaggerAutogen({ language: 'zh-CN', openapi: '3.0.0' })(outputFile, endpointsFiles, doc);
+swaggerAutogen({ openapi: '3.0.0' })(outputFile, endpointsFiles, doc);
