ea75c675440fab901168c226dbd06737766ce7f7
Lawrence Li <lonor@live.com>
2022-04-23 21:43:18 +0800

    module: debug

--- swagger/serve.js
+++ swagger/serve.js
@@ -1,9 +1,10 @@
 import swaggerUiExpress from 'swagger-ui-express';
+console.log("start create require");
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 // or use nodejs 17: import doc from './swagger-output.json' assert { type: 'json' };
 const swaggerJSON = require('./output.json');
-
+console.log("end create require");
 const swagger = swaggerUiExpress.setup(swaggerJSON);
 
 export default swagger; // for get endpoint
